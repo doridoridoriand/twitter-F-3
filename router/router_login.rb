fb6afd8f2ccb4b40a44ebc8f20c63674409bf1dc
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-12 17:00:47 +0900

    ログイン部分を作成

--- router/router_login.rb
+++ router/router_login.rb
@@ -1,5 +1,20 @@
 class RouterLogin < Sinatra::Base
 
   post '/login' do
+    posted_data = JSON.parse request.body.read
+    if ServiceUser.exist?(posted_data['name']) && ServiceUser.authenticate(posted_data['name'], posted_data['password'])
+      user_uuid  = ServiceUser.find_by_username(posted_data['name']).first
+      user_token = ServiceUserTokens.create(user_uuid.uuid)
+      content_type :json, charset: 'utf-8'
+      {
+        "result": true,
+        "data": {
+          "token": user_token
+        }
+      }.to_json
+    else
+      content_type :json, charset: 'utf-8'
+      {"error": LOGIN_INFORMATION_HAS_WRONG}.to_json(root: false)
+    end
   end
 end
--- router/router_register.rb
+++ router/router_register.rb
@@ -1,5 +1,25 @@
 class RouterRegister < Sinatra::Base
 
   post '/register' do
+    posted_data = JSON.parse request.body.read
+    if posted_data.has_problems?
+
+      content_type :json, charset: 'utf-8'
+      {"error": WRONG_FORMAT}.to_json(root: false)
+    else
+      unless ServiceUser.duplicate(posted_data['name'])
+        user_token = ServiceUserTokens.create(ServiceUser.create(posted_data))
+        content_type :json, charset: 'utf-8'
+        {
+          "result": true,
+          "data": {
+            "token": user_token
+          }
+        }.to_json
+      else
+        content_type :json, charset: 'utf-8'
+        {"error": USER_ID_ALREADY_IN_USE}.to_json(root: false)
+      end
+    end
   end
 end
