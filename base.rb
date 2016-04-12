51769b83d5038cea570d33c4b23f566b500388cc
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-12 16:58:40 +0900

    modelとserviceとhelperを追加

--- base.rb
+++ base.rb
@@ -5,12 +5,34 @@ require 'open-uri'
 require 'json'
 require 'bcrypt'
 
-# load Service
-$: << File.expand_path(File.join(__FILE__, '..', 'service'))
+# load lib
+$: << File.expand_path(File.join(__FILE__, '..', 'lib'))
+require 'constants'
 
-db_config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'database.yml')))
+# load model
+$: << File.expand_path(File.join(__FILE__, '..', 'model'))
+require 'item'
+require 'user'
+require 'user_token'
+
+# load service
+$: << File.expand_path(File.join(__FILE__, '..', 'service'))
+require 'service_item'
+require 'service_user'
+require 'service_user_token'
+
+# load helper
+$: << File.expand_path(File.join(__FILE__, '..', 'helper'))
+require 'validator'
+require 'security'
+
+db_config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', 'config', 'database.yml')))
 
 ActiveRecord::Base.establish_connection(db_config['db']['production'])
+
+include Constants
+include Validator
+include Security
 
 module Base
 end
