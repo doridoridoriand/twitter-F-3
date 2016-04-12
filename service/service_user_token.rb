f8b5f991d65c031fa3753c2d3308367ad82c8e1a
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-12 17:50:54 +0900

    token用のmodelとserviceを用意

---
+++ service/service_user_token.rb
@@ -0,0 +1,14 @@
+class ServiceUserTokens < UserToken
+
+  # User Tokenに関してはその性質上、作成と削除しか叩けない
+
+  def self.create(params)
+    user_token = SecureRandom.hex(10)
+    UserToken.create(uuid:  params,
+                     token: user_token)
+    user_token
+  end
+
+  def self.delete(params)
+  end
+end
