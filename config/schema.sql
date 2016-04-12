e9cb1b2011ed008476df1ca441eacccd931caa68
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-12 17:54:55 +0900

    DBスキーマを更新

--- config/schema.sql
+++ config/schema.sql
@@ -1,11 +1,19 @@
 create database if not exists `teamf_twitter`;
 
 create table teamf_twitter.users (
-  `id`            int(30)      unsigned not null auto_increment,
-  `uuid`          varchar(255) not null,
-  `user_id`       varchar(255) not null,
-  `password_hash` varchar(255) not null,
-  `password_salt` varchar(255) not null
+  `id`            int(30)       unsigned not null auto_increment,
+  `uuid`          varchar(768)  not null,
+  `user_id`       varchar(255)  not null,
+  `screen_name`   varchar(255)  not null default 'no name',
+  `password_hash` varchar(1024) not null,
+  `password_salt` varchar(1024) not null,
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
+
+create table teamf_twitter.user_tokens (
+  `id`    int(30)      unsigned not null auto_increment,
+  `uuid`  varchar(255) not null,
+  `token` varchar(255) not null,
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
 
@@ -14,7 +22,7 @@ create table teamf_twitter.items (
   `uuid`       varchar(255)  not null,
   `hex`        varchar(255)  not null,
   `content`    varchar(255)  not null,
-  `created_at` varchar( 255) timestamp not null default 0,
-  `updated_at` varchar(255)  not null default current_timestamp on update current_timestamp,
+  `created_at` timestamp     not null default 0,
+  `updated_at` timestamp     not null default current_timestamp on update current_timestamp,
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
