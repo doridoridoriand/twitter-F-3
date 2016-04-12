6b9755f5f56c77ad57aa069ea26cf40c5e703739
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-12 09:25:18 +0900

    バリデーション用のhelperを用意

---
+++ helper/validator.rb
@@ -0,0 +1,18 @@
+$: << File.expand_path(File.join(__FILE__, '..', 'lib')) 
+require 'constants'
+
+include Constants
+
+module Validator
+
+  # @param  contents
+  # @return contents or Raise Error
+  def is_140?
+    unless self.length.to_i <= 140
+      raise 'ContentsOver140'
+    else
+      self
+    end
+  end
+
+end
