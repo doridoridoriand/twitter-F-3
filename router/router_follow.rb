3dabf5385e3216520125b7954f40d1515207fc10
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-14 19:08:57 +0900

    [WIP] フォローアンフォロー、フォロワー一覧取得作成中

--- router/router_follow.rb
+++ router/router_follow.rb
@@ -1,5 +1,20 @@
 class RouterFollow < Sinatra::Base
 
-  post '/post' do
+  post '/follow' do
+    authorized_user_area!
+    posted_data = JSON.parse request.body.read
+    hash = {}
+    hash['uuid'] = authorized_user_uuid
+    hash['target_uuid'] = posted_data['token']
+    ServiceFollower.create(hash)
+  end
+
+  post '/unfollow' do
+  end
+
+  get '/following' do
+  end
+
+  get '/followers' do
   end
 end
