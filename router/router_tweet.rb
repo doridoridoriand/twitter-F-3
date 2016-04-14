ced1a37b223ae7d10dfb46755146723040aa5749
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-14 13:49:02 +0900

    検証用のタイムライン表示を実装。ページングを実装

--- router/router_tweet.rb
+++ router/router_tweet.rb
@@ -2,7 +2,19 @@ class RouterTweet < Sinatra::Base
 
   # APIのルートにGETでアクセスするとタイムライン一覧を取得するという仕様なのでそれに従う
   get '/' do
-    "teamF_twitterAPI"
+    number_of_items = {}
+    unless params['amounts'] && params['page']
+      number_of_items['from'] = 0
+      number_of_items['to']   = 100
+    else
+      content_type :json, charset: 'utf-8'
+      amounts = params['amounts'].to_i
+      page    = params['page'].to_i
+      number_of_items['from'] = page == 0 ? 0 : amounts * page + 1
+      number_of_items['to']   = amounts * page + amounts
+    end
+      content_type :json, charset: 'utf-8'
+      ServiceItem.show(number_of_items).to_json(root: false)
   end
 
   post '/tweet' do
