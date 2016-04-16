4aa922a9ecc290ace468e0c57d09714a956c03b6
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-17 00:22:31 +0900

    タイムライン表示をキレイにするhelperを叩くように変更。JSONの形が仕様書と異なっていたので修正

--- router/router_tweet.rb
+++ router/router_tweet.rb
@@ -14,15 +14,16 @@ class RouterTweet < Sinatra::Base
       number_of_items['to']   = amounts * page + amounts
     end
       content_type :json, charset: 'utf-8'
-      ServiceItem.show(number_of_items).to_json(root: false)
+      data = ServiceItem.show(number_of_items)
+      data.to_tl.to_json
   end
 
   post '/tweet' do
     authorized_user_area!
     posted_data = JSON.parse request.body.read
-    if posted_data["text"].less_than_140?
+    if posted_data["tweet"].less_than_140?
       params['uuid'] = authorized_user_uuid
-      params['content'] = posted_data["text"]
+      params['content'] = posted_data["tweet"]
       ServiceItem.create(params)
     else
       content_type :json, charset: 'utf-8'
@@ -30,6 +31,9 @@ class RouterTweet < Sinatra::Base
     end
   end
 
-  get '/tweet/:tweet_id' do
+  get '/:uuid/:hex' do
+    data = ServiceItem.find_by_uuid_hex(params)
+    content_type :json, charset: 'utf-8'
+    data.to_json(root: false)
   end
 end
