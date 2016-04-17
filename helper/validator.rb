017787891a6cc3656a2d0b82765a60f34b7f6d3a
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-17 17:45:31 +0900

    返り値をbooleanにしたことの記載が漏れていたので追記

--- helper/validator.rb
+++ helper/validator.rb
@@ -1,12 +1,7 @@
-$: << File.expand_path(File.join(__FILE__, '..', 'lib')) 
-require 'constants'
-
-include Constants
-
 module Validator
 
   # @param  contents
-  # @return contents or Raise Error
+  # @return boolean
   def less_than_140?
     unless self.length.to_i <= 140
       false
