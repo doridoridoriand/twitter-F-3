a76577ef75eafe3b881c1501af7c2e8d037a3ebf
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-19 15:58:06 +0900

    JSONの形を変更

--- helper/timeline.rb
+++ helper/timeline.rb
@@ -5,6 +5,8 @@ module Timeline
       user_info                  = ServiceUser.find_by_uuid(entry.uuid).first
       hash[i][:user_id]          = user_info.user_id
       hash[i][:screen_name]      = user_info.screen_name
+      hash[i][:uuid]             = user_info.uuid
+      hash[i][:hex]              = entry.hex
       hash[i][:content]          = entry.content
       hash[i][:tweet_detail_url] = "#{ROOT_URL}/#{entry.uuid}/#{entry.hex}"
       hash[i][:created_at]       = entry.created_at
