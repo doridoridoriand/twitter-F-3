d29a82299105e1cf04f314d88d043fdbb538c921
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-23 18:39:12 +0900

    クソみたいなミスをしていたので修正

--- helper/timeline.rb
+++ helper/timeline.rb
@@ -9,9 +9,9 @@ module Timeline
       hash[i][:uuid]             = user_info.uuid
       hash[i][:hex]              = entry.hex
       hash[i][:content]          = entry.content
-      hash[i][:tweet_detail_url] = "#{ROOT_URL}/#{entry.uuid}/#{entry.hex}",
-      hash[i][:like_count]       = entry.like_count,
-      hash[i][:retweet_count]    = entry.retweet_count,
+      hash[i][:tweet_detail_url] = "#{ROOT_URL}/#{entry.uuid}/#{entry.hex}"
+      hash[i][:like_count]       = entry.like_count
+      hash[i][:retweet_count]    = entry.retweet_count
       hash[i][:created_at]       = entry.created_at
     end
     hash.values
