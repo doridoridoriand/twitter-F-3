cabda4e0ac2acf3bc5432f4b4a0ba52b96504053
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-04-25 07:12:28 +0000

    通知ようのエンドポイントを追加

--- router/router_notification.rb
+++ router/router_notification.rb
@@ -1,2 +1,9 @@
 class RouterNotification < Sinatra::Base
+
+  get '/notification' do
+    authorized_user_area!
+    content_type :json, charset: 'utf-8'
+    ServiceNotification.find_by_uuid(authorized_user_uuid).to_notify_tl.to_json(root: false)
+  end
 end
+
